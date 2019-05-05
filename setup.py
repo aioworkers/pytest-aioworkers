d23c5c6462dd71934f9870b3c893c1facf03e724
Alexander Malev <yttrium@somedev.ru>
2019-05-05 23:41:04 +0300

    0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='pytest-aioworkers',
-    version='0.1.0',
+    version='0.2.0',
     author='Alexander Malev',
     author_email='malev@somedev.ru',
     maintainer='Alexander Malev',
