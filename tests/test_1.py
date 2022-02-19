d7a1fffa02fed133273a71e33cb9c481d2913b51
Alexander Malev <malev@somedev.ru>
2022-02-19 15:54:22 +0300

    tests with black

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,4 +1,3 @@
-
 async def test_make_coro(make_coro):
     coro = make_coro(1)
     assert 1 == await coro()
--- tests/test_config.py
+++ tests/test_config.py
@@ -3,9 +3,12 @@ import pytest
 
 @pytest.fixture
 def config_yaml(config_yaml):
-    return config_yaml + """---
+    return (
+        config_yaml
+        + """---
     s: 2
     """
+    )
 
 
 def test_cfg1(config):
