fe40b09f96156961e36acc147b19f1751e760726
Alexander Malev <malev@somedev.ru>
2022-02-19 15:08:11 +0300

    refact module as package

---
+++ pytest_aioworkers/utils.py
@@ -0,0 +1,25 @@
+from typing import Dict, Iterable
+
+
+try:
+    import yaml
+except ImportError:
+    _yaml_loader = None
+else:
+    _yaml_loader = getattr(yaml, "CLoader", yaml.Loader)
+
+
+def _yaml_import_error(s: str) -> Iterable[Dict]:
+    import yaml  # noqa
+
+    return yaml_load_all(s)
+
+
+def _yaml_load_all(s: str) -> Iterable[Dict]:
+    yield from yaml.load_all(s, Loader=_yaml_loader)
+
+
+if _yaml_loader is None:
+    yaml_load_all = _yaml_import_error
+else:
+    yaml_load_all = _yaml_load_all
