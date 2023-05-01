cf144b0afa4f2373569494aed06ba64858ef99ed
Alexander Malev <malev@somedev.ru>
2023-05-01 18:21:52 +0300

    Fixture as class

--- pytest_aioworkers/__init__.py
+++ pytest_aioworkers/__init__.py
@@ -1,13 +1,19 @@
-from collections import namedtuple
+from typing import MutableSet, NamedTuple
 
 __version__ = "0.0.0"
 
 
-AioWorkers = namedtuple("AioWorkers", "plugins groups")
-Groups = namedtuple("Groups", "include exclude")
+class Groups(NamedTuple):
+    include: MutableSet[str]
+    exclude: MutableSet[str]
 
 
 class Plugins(list):
     def __init__(self, *args):
         super().__init__(*args)
         self.default = False
+
+
+class AioWorkers(NamedTuple):
+    plugins: Plugins
+    groups: Groups
